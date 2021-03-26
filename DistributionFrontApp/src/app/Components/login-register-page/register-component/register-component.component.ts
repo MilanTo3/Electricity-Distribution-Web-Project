a716c4b0dd1aae72843847984ce311b1ce030b77
mtomin367@gmail.com <mtomin367@gmail.com>
2021-03-26 11:01:42 +0100

    Mockup izgled pocetne strane projekta. Dizajn i organizacija fajlova.

---
+++ DistributionFrontApp/src/app/Components/login-register-page/register-component/register-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-component',
+  templateUrl: './register-component.component.html',
+  styleUrls: ['./register-component.component.css']
+})
+export class RegisterComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- DistributionFrontApp/src/app/app.module.ts
+++ DistributionFrontApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './Components/login-register-page/login-component/login-component.component';
+import { RegisterComponentComponent } from './Components/login-register-page/register-component/register-component.component';
+import { LoginRegisterPageComponent } from './Components/login-register-page/login-register-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegisterPageComponent,
+    LoginComponentComponent,
+    RegisterComponentComponent
   ],
   imports: [
     BrowserModule,
