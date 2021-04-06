8a24daf51d966eb89e9b61fc6afc01eeefb2a7a0
tammtry <tammtry@users.noreply.github.com>
2021-04-06 17:35:59 +0200

    [routing] proba

--- DistributionFrontApp/src/app/Components/login-register-page/login-component/login-component.component.ts
+++ DistributionFrontApp/src/app/Components/login-register-page/login-component/login-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-component',
@@ -7,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
   
-
+  openDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
+  
 }
--- DistributionFrontApp/src/app/app-routing.module.ts
+++ DistributionFrontApp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardPageComponent } from './Components/dashboard-page/dashboard-page.component';
+import { LoginRegisterPageComponent } from './Components/login-register-page/login-register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: LoginRegisterPageComponent },
+  { path: 'dashboard', component: DashboardPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
