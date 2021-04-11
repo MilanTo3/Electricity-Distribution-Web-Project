7ad1a3771ef351fdedde45b5b7c34fdebf045ee6
Maja Ozegovic <ozegovicbiz@gmail.com>
2021-04-11 17:27:34 +0200

    generated new call component

---
+++ DistributionFrontApp/src/app/Components/new-call/new-call.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-call',
+  templateUrl: './new-call.component.html',
+  styleUrls: ['./new-call.component.css']
+})
+export class NewCallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
