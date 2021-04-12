bca7c5717636b2811a52f076d3e69f2ed376df80
tammtry <tammtry@users.noreply.github.com>
2021-04-12 11:52:11 +0200

    Route to existing 'new call' component from 'Calls' in MyIncidents side-bar menu.

--- DistributionFrontApp/src/app/Components/dashboard-page/table-dashboard/new-incident/calls/calls.component.ts
+++ DistributionFrontApp/src/app/Components/dashboard-page/table-dashboard/new-incident/calls/calls.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-calls',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CallsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  CreateNewCall() {
+    this.router.navigate(['/new-call']);
   }
 
 }
