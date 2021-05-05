bb5c1f64ee272f6a2e46cd720e0ffc04f087cd13
Maja Ozegovic <ozegovicbiz@gmail.com>
2021-05-05 16:27:40 +0200

    work plan form done

--- DistributionFrontApp/src/app/Components/work-plans/work-plan-form/work-plan-form.component.ts
+++ DistributionFrontApp/src/app/Components/work-plans/work-plan-form/work-plan-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Localbase from 'localbase';
+import { WorkPlanWrapper } from '../../../Models/formWrappers';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-work-plan-form',
@@ -6,10 +10,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-plan-form.component.css']
 })
 export class WorkPlanFormComponent implements OnInit {
-
-  constructor() { }
+  db = new Localbase('db');
+  wrapper: WorkPlanWrapper = new WorkPlanWrapper();
+  constructor(private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     sessionStorage.clear();
+    this.db.collection('images').delete();
+    this.router.navigateByUrl('/newWorkPlan/basic-information');
+  }
+
+  onSubmit() {
+
+    let check = JSON.parse(sessionStorage.getItem("planBasicInfoFormValid"));
+    if(check === false || check === null){
+      this.router.navigateByUrl('/newWorkPlan/basic-information');
+      this.showToastrWarning();
+      return;
+    }
+    console.log(check);
+    this.setInfoForm();
+    this.setHistoryForm();
+    this.setMediaForm();
+    this.setInstructionsForm();
+    console.log(this.wrapper);
+    this.showToastrSuccess();
+    this.router.navigateByUrl('/workPlans');
+
+  }
+
+  showToastrSuccess(){
+    
+    this.toastr.success('Your work plan has been successfuly sent.', 'Form successfuly sent.');
+
+  }
+
+  showToastrWarning(){
+    this.toastr.warning('Seems like theres an error with basic information form input. Please check again.', 'Ooops!');
+
+  }
+
+  setInfoForm(){
+    let formdata = JSON.parse(sessionStorage.getItem("planBasicInfoForm"));
+    this.wrapper.basicInformationForm = formdata;
+  }
+
+  setHistoryForm(){
+    let formdata = JSON.parse(sessionStorage.getItem("historyStateForm"));
+    this.wrapper.historyForm = formdata;
+  }
+
+  setMediaForm(){
+    this.wrapper.mediaForm = [];
+    this.db.collection('images').get().then(x => {
+      let i;
+      for (i = 0; i < x.length; i++) {
+        this.wrapper.mediaForm.push(x[i]);
+      }
+
+    });
+  }
+  setInstructionsForm(){
+    let formdata = JSON.parse(sessionStorage.getItem("WPSwitchingInstuctionsForm"));
+    this.wrapper.switchingInstructionsForm = formdata;
   }
 }
