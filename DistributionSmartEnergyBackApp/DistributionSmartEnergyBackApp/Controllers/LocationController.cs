dd45329cdd18479984a1cdf10eeaf88c56e9bac2
Maja Ozegovic <ozegovicbiz@gmail.com>
2021-05-17 19:08:06 +0200

    location backend

---
+++ DistributionSmartEnergyBackApp/DistributionSmartEnergyBackApp/Controllers/LocationController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DistributionSmartEnergyBackApp.Models;
+using DistributionSmartEnergyBackApp.Models.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace DistributionSmartEnergyBackApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+
+        private readonly ILocation _context;
+        public LocationController(ILocation context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Location/id
+        [HttpGet("{id}")]
+        [Route("GetLocation")]
+        public async Task<LocationModel> GetLocation(long id)
+        {
+            return await _context.GetLocation(id);
+        }
+
+        // GET: api/Location/GetLocations
+        [HttpGet]
+        [Route("GetLocations")]
+        public async Task<IEnumerable<LocationModel>> GetLocations()
+        { 
+             return await _context.GetLocations();
+        }
+
+        // POST: api/Location/AddLocation
+        [HttpPost]
+        [Route("AddLocation")]
+        public async Task<IActionResult> AddLocation([FromBody]LocationModel location)
+        {
+            try
+            {
+                await _context.AddLocation(location);
+                return Ok();
+
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        // PUT api/Location/5
+        [HttpPut("{id}")]
+        [Route("Update")]
+        public async Task Put([FromBody] LocationModel location)
+        {
+            try
+            {
+                await _context.UpdateLocation(location);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error while updating a location. [{e.Message}]");
+            }
+        }
+
+
+        //DELETE api/Location/Delete/5
+        [HttpDelete("Delete/{id}")]
+        public async Task DeleteLocation(long id)
+        {
+            try
+            {
+                await _context.DeleteLocation(id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error while deleting a location. [{e.Message}]");
+            }
+        }
+
+
+    }
+}
---
+++ DistributionSmartEnergyBackApp/DistributionSmartEnergyBackApp/Services/LocationService.cs
@@ -0,0 +1,52 @@
+﻿using DistributionSmartEnergyBackApp.Models;
+using DistributionSmartEnergyBackApp.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DistributionSmartEnergyBackApp.Services
+{
+    public class LocationService : ILocation
+    {
+        private readonly AuthenticationContext _context;
+
+        public LocationService(AuthenticationContext context)
+        {
+            _context = context;
+        }
+        public async Task AddLocation(LocationModel location)
+        {
+            _context.Locations.Add(location);
+            await _context.SaveChangesAsync(true);
+        }
+
+        public async Task DeleteLocation(long id)
+        {
+            var loc = await _context.Locations.FindAsync(id);
+
+            _context.Locations.Remove(loc);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<LocationModel> GetLocation(long id)
+        {
+            return await _context.Locations.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<LocationModel>> GetLocations()
+        {
+            return await _context.Locations.ToListAsync();
+        }
+
+        public async Task UpdateLocation(LocationModel location)
+        {
+            _context.Locations.Update(location);
+            await _context.SaveChangesAsync();
+           
+
+        }
+    }
+}
