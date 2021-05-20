28dc2dcc8a52f2724ff7de3c2848e84ca7cc4737
mtomin367@gmail.com <mtomin367@gmail.com>
2021-05-20 02:42:43 +0200

    work request can be viewed. History table needs some tweaks tho. Oh well, we'll see about that tommorow.

--- DistributionFrontApp/src/app/Components/work-requests-page/work-request-form/history-state-changes/history-state-changes.component.ts
+++ DistributionFrontApp/src/app/Components/work-requests-page/work-request-form/history-state-changes/history-state-changes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { TableComponentComponent } from 'src/app/Components/admin-profile-requests/table-component/table-component.component';
 import { HistoryStateChange } from '../../../../Models/HistoryStateChange.model';
 
@@ -9,38 +10,45 @@ import { HistoryStateChange } from '../../../../Models/HistoryStateChange.model'
 })
 export class HistoryStateChangesComponent implements OnInit, AfterViewInit {
 
-  @ViewChild(TableComponentComponent, {static: false}) table: TableComponentComponent;
+  @ViewChild(TableComponentComponent, { static: false }) table: TableComponentComponent;
   stateArray: HistoryStateChange[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-  
+
   }
 
-  ngAfterViewInit(){
-    if(sessionStorage.getItem("historyStateForm") !== null){
+  ngAfterViewInit() {
+    if (sessionStorage.getItem("historyStateForm") !== null) {
       let addedStates = JSON.parse(sessionStorage.getItem("historyStateForm"));
       this.stateArray = this.stateArray.concat(addedStates);
-      this.table.dataBind.data = this.table.dataBind.data.concat(this.stateArray);
+      this.table.dataToPrint = this.stateArray;
+      this.table.dataBind.data = this.table.dataToPrint;
+    } else if(this.table.dataBind.data.length == 0){
+      let item = new HistoryStateChange('Pera', 'Peric', new Date(), 'State changed to canceled.');
+      this.table.dataBind = new MatTableDataSource(this.table.dataToPrint);
+      this.table.keyNames = Object.getOwnPropertyNames(item);
+      this.table.enableView();
     }
+
   }
 
-  addState(state: number){
-  
+  addState(state: number) {
+
     let item;
-    if(state === 0){
-      item = new HistoryStateChange('Pera', 'Peric', new Date(2003, 12, 21, 23, 22, 24), 'State changed to canceled.');
-    }else if(state === 1){
-      item = new HistoryStateChange('Pera', 'Peric', new Date(2003, 12, 21, 23, 22, 24), 'State changed to denied.');
-    }else{
-      item = new HistoryStateChange('Pera', 'Peric', new Date(2003, 12, 21, 23, 22, 24), 'State changed to approved');
+    if (state === 0) {
+      item = new HistoryStateChange('Pera', 'Peric', new Date(), 'State changed to canceled.');
+    } else if (state === 1) {
+      item = new HistoryStateChange('Pera', 'Peric', new Date(), 'State changed to denied.');
+    } else {
+      item = new HistoryStateChange('Pera', 'Peric', new Date(), 'State changed to approved');
     }
 
     this.stateArray.push(item);
     sessionStorage.setItem("historyStateForm", JSON.stringify(this.stateArray));
-    this.table.dataBind.data.push(item);
-    this.table.dataBind.data = this.table.dataBind.data; // Why does this work, i mean like wut. Just wut.
+    this.table.dataToPrint.push(item);
+    this.table.dataBind.data = this.table.dataBind.data;
 
   }
 
