f76d8d3588dd4e14e45c192a1973b12715bdc2ae
mtomin367@gmail.com <mtomin367@gmail.com>
2021-06-03 20:21:11 +0200

    bruh moment

---
+++ DistributionSmartEnergyBackApp/DistributionSmartEnergyBackApp/Models/EntityModels/SettingsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DistributionSmartEnergyBackApp.Models.EntityModels
+{
+    public class SettingsModel
+    {
+        // Prakticno ideja je imati tabelu Settings, koja bi imala jedan entry za svakog admina.
+        // sastojala bi se od ikonica i ostalih info.
+        // Ikonice bi imale defaultnu vrednost neku, na njihovo mesto bi se upload-ovale custom.
+        private string username; // za koga je settings.
+        private string icon1;
+        private string icon2;
+        private string icon3;
+        // + other stuffs, ya know
+
+    }
+}
