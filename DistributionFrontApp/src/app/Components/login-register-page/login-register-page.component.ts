9117a7e9a905eda475640495fce855814205be5f
mtomin367@gmail.com <mtomin367@gmail.com>
2021-06-05 05:58:12 +0200

    front google register.

--- DistributionFrontApp/src/app/Components/login-register-page/login-register-page.component.ts
+++ DistributionFrontApp/src/app/Components/login-register-page/login-register-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login-register-page',
@@ -10,8 +11,10 @@ export class LoginRegisterPageComponent implements OnInit {
 
   show = true;
   toggleFormsVisibility = false;
+  auth2: any;
+  @ViewChild('googleRef', {static: true }) regEl: ElementRef;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     if(sessionStorage.getItem('loggedUser') != null){
@@ -25,6 +28,41 @@ export class LoginRegisterPageComponent implements OnInit {
     this.show = !this.show;
  
   }
+
+  googleInitialize() {
+    window['googleSDKLoaded'] = () => {
+      window['gapi'].load('auth2', () => {
+        this.auth2 = window['gapi'].auth2.init({
+          client_id: '294447893252-04ntit75kidujm538u3q9bk8es5v1b9p.apps.googleusercontent.com',
+          cookie_policy: 'single_host_origin',
+          scope: 'profile email'
+        });
+        this.prepareRegister();
+      });
+    }
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'google-jssdk'));
+  }
   
+  prepareRegister(){
+
+    let name;
+    this.auth2.attachClickHandler(this.regEl.nativeElement, {},
+      (googleUser) => {
+        let profile = googleUser.getBasicProfile();
+        console.log('Token || ' + googleUser.getAuthResponse().id_token);
+        console.log(profile);
+        this.show = true;
+
+      }, (error) => {
+        this.toastr.error('Eh? Seems like there\'s been an error getting account info.', 'Error');
+      });
+
+  }
 
 }
