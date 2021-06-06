7464a8420e1c5d48797227aae3f3be81410245f1
mtomin367@gmail.com <mtomin367@gmail.com>
2021-06-06 13:25:06 +0200

    namestena mapa sa call-ovima. well spank my ass and call me charlie, if i aint nearly done.

--- DistributionFrontApp/src/app/Components/map-page/map-component/map-component.component.ts
+++ DistributionFrontApp/src/app/Components/map-page/map-component/map-component.component.ts
@@ -38,15 +38,21 @@ export class MapComponentComponent implements OnInit {
 
   constructor(private callsGetter: CallService, private locationGetter: LocationService) { }
 
-  addPoint(lat: number, lon: number, additionalInfo: string) {
+  addPoint(lat: number, lon: number, additionalInfo: string, type: string) {
 
+    console.log(additionalInfo);
     let newPoint = new Feature({ geometry: new Point(fromLonLat([lon, lat])) });
     newPoint.set('addInfo', additionalInfo);
+    newPoint.set('type', type);
+    let picturePath: string = "assets/Images/pin.png";
+    if(type.startsWith('Call')){
+      picturePath = "assets/Images/phone-call.svg";
+    }
 
     newPoint.setStyle(new Style({
       image: new Icon(({
         crossOrigin: 'anonymous',
-        src: 'assets/Images/pin.png',
+        src: picturePath,
         imgSize: [512, 512],
         scale: 0.07,
       })),
@@ -98,17 +104,21 @@ export class MapComponentComponent implements OnInit {
     });
 
     this.map.addLayer(this.vectorLayer);
-    this.addPoint(45.2396, 19.8227, "");
-    this.addPoint(45.2396, 19.9227, "");
+    this.addPoint(45.2396, 19.8227, "", "");
+    this.addPoint(45.2396, 19.9227, "", "");
     let calls = await this.callsGetter.GetCalls().toPromise();
+
     let i;
-    for(i = 0; i < calls["Length"]; i++){
-      let location = await this.locationGetter.GetLocation(calls[i]["id"]);
-      console.log(location);
-      this.addPoint(location["latitude"], location["longitude"], calls[i]["reason"]);
+    for(i = 0; i < calls["length"]; i++){
+      let location = await this.locationGetter.GetLocation(calls[i]["id"]).toPromise();
+      if(location !== null){
+        this.addPoint(location["latitude"], location["longitude"], calls[i]["reason"], "Call in street: " + location["street"]);
+      }
     }
     
     this.overlayPopup.setElement(document.getElementById('tooltip'));
+    let popupContent = document.getElementById('popupContent');
+    let popupCloser = document.getElementById('popupCloser');
 
     this.map.on('click', (evt) => {
       let item = document.getElementById('tooltip');
@@ -119,9 +129,11 @@ export class MapComponentComponent implements OnInit {
       );
       if (feature) {
         let info = feature.get('addInfo');
+        let type = feature.get('type');
         this.overlayPopup.setPosition(evt.coordinate);
         item.hidden = false;
-        item.innerHTML = "<p>"+ info +"</p>";
+        popupContent.innerHTML = "<p>Reason: "+ info +"</p>";
+        popupCloser.innerHTML = "<p>"+ type +"</p>";
       }else{
         item.hidden = true;
         this.overlayPopup.setPosition(undefined);
