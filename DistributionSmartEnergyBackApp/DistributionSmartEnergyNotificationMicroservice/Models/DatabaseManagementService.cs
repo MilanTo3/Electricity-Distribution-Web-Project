737b86dae5dd6d7fa3ae66e3584d1586237e7942
MilanTo3 <mtomin367@gmail.com>
2021-07-05 09:33:12 +0200

    Added notification microservice project and docker fot it.

---
+++ DistributionSmartEnergyBackApp/DistributionSmartEnergyNotificationMicroservice/Models/DatabaseManagementService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DistributionSmartEnergyNotificationMicroservice.Models
+{
+    public static class DatabaseManagementService
+    {
+        public static void MigrationInitialization(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<AuthenticationContext>().Database.Migrate();
+            }
+        }
+    }
+}
